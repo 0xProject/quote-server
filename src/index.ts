ccfba924a72819273318f53fd21fcc2ecd357f87
Phil Liao <phil@0x.org>
2021-06-01 10:46:51 -0500

    export the schema property

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export { SignedOrder } from '@0x/order-utils';
 export { serverRoutes } from './router';
-export * from './types';
+export { schemas } from './schemas';
 export {
     TakerRequest,
     Quoter,
